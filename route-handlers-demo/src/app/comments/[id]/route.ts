75294e294460cb36ade2b2a5e5a22863d0101278
martinromario55 <martinromario55@gmail.com>
2024-04-28 06:24:49 +0300

    Search query and Redirect

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,3 +1,4 @@
+import { redirect } from 'next/navigation'
 import { comments } from '../data'
 
 // Get one Item by ID
@@ -5,6 +6,7 @@ export async function GET(
   _request: Request,
   { params }: { params: { id: string } }
 ) {
+  if (parseInt(params.id) > comments.length) redirect('/comments')
   const comment = comments.find(comment => comment.id === parseInt(params.id))
   return Response.json(comment)
 }
--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,9 +1,17 @@
+import { NextRequest } from 'next/server'
 import { comments } from './data'
 
-export async function GET() {
-  return Response.json(comments)
+// Get all Items
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get('query')
+  const filteredComments = query
+    ? comments.filter(comment => comment.text.includes(query))
+    : comments
+  return Response.json(filteredComments)
 }
 
+// Create new Item
 export async function POST(request: Request) {
   const comment = await request.json()
   const newComment = {
