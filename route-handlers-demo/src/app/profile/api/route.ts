d8248d2f3efaad3730027f610bc7f29e9383917e
martinromario55 <martinromario55@gmail.com>
2024-04-29 20:13:52 +0300

    Handling Routes in Headers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,12 @@
-export async function GET() {
-  return new Response('Profile API data')
+import { headers } from 'next/headers'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers)
+  const headerList = headers()
+  console.log('Headers:', requestHeaders.get('Authorization'))
+  console.log('Header2:', headerList.get('Authorization'))
+  return new Response('<h1>Profile API data</h1>', {
+    headers: { 'Content-Type': 'text/html' },
+  })
 }
